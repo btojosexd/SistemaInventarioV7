6b17dd73abd4c8b3e7ff7d3aa274541607e5a1cc
jose bracamonte <btojosexd@gmail.com>
2023-08-25 22:19:27 -0400

    Configuracion del proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int IdBodega { get; set; }
+        [Required(ErrorMessage = "Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage = "El numero maximo de caracteres es 60")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es Requerido")]
+        [MaxLength(100, ErrorMessage = "El numero maximo de caracteres es 100")]
+        public string Descripcion { get; set; }
+        [Required]
+        public bool Estado { get; set; }
+
+    }
+}
